a278507f2ec5b9abbfb2467e69db83b1c051755b
dukehammer <jdwhite89@gmail.com>
2017-06-11 12:04:52 -0500

    testing more with url

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+/*
 var newBase = document.createElement("base");
 newBase.setAttribute("href", document.location.hostname);
 newBase.setAttribute("target", "_blank");
 document.getElementsByTagName("head")[0].appendChild(newBase);
+console.info("Changed baseurl");
+*/
