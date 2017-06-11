82e75b12a4e37963c50ff0407664c4769851352c
dukehammer <jdwhite89@gmail.com>
2017-06-11 17:06:31 -0500

    Added .css file, updating main project page format.

--- index.js
+++ index.js
@@ -1,7 +1,1 @@
-/*
-var newBase = document.createElement("base");
-newBase.setAttribute("href", document.location.hostname);
-newBase.setAttribute("target", "_blank");
-document.getElementsByTagName("head")[0].appendChild(newBase);
-console.info("Changed baseurl");
-*/
+/* No js needed yet */
